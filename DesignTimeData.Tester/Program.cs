6328fbb418c67f82255265ae1d6da3dc82ca5213
James Montemagno <james.montemagno@gmail.com>
2019-09-03 17:25:55 -0700

    Add test projects

---
+++ DesignTimeData.Tester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DesignTimeData.Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Xamarin.Forms.DesignTimeData.IsEnabled = true;
+
+            Console.WriteLine(Xamarin.Forms.DesignMode.IsDesignModeEnabled.ToString());
+            Console.ReadLine();
+        }
+    }
+}
